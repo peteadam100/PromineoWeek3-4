92a28dde70c5a70078ebcab1ab8e3e3c5dfe3610
Adam Petersen <silverbadger@gmail.com>
2023-01-30 20:42:27 -0700

    added more cosmetics for the pauses between questions

--- src/assignment/Week4Assignment.java
+++ src/assignment/Week4Assignment.java
@@ -5,35 +5,50 @@ import java.util.Arrays;
 
 public class Week4Assignment {
 	public static void main(String[] args) throws IOException {
-		boolean pauses = false;
+		boolean pauses = true;
 		//1
 		//setup initial array
 		int[] ages = {3,9,23,64,2,8,28,93};
 		//1.a
-		System.out.println("1.a \n ---Array 1 ages subtracted: \n\t" + SubtractAges(ages));
+		System.out.println("1.a \n ---Array 1 ages subtracted:");
+		System.out.println("\tArray used: " + Arrays.toString(ages));
+		System.out.println("\tResult: " + SubtractAges(ages));
+		System.out.print("   ----- Press Enter to continue-----");
 		if (pauses) {System.in.read();}
 		
 		//1.b
 		//created new array for adding a new value
 		int[] ages2 = {3,9,23,64,2,8,28,93,100};
-		System.out.println("1.b \n ---Array 2 ages subtracted: \n\t" + SubtractAges(ages2));
+		System.out.println("1.b \n ---Array 2 ages subtracted:");
+		System.out.println("\tArray used: " + Arrays.toString(ages2));
+		System.out.println("\tResult: " + SubtractAges(ages2));
+		System.out.print("   ----- Press Enter to continue-----");
 		if (pauses) {System.in.read();}
 		if (pauses) {System.in.read();};
 		
 		//1.c
-		System.out.println("1.c \n ---Average of numbers in age array: \n\t" + CalculateAverageAges(ages2));
+		System.out.println("1.c \n ---Average of numbers in age array:");
+		System.out.println("\tArray used: " + Arrays.toString(ages2));
+		System.out.println("\tResult: " + CalculateAverageAges(ages2));
+		System.out.print("   ----- Press Enter to continue-----");
 		if (pauses) {System.in.read();}
 		if (pauses) {System.in.read();}
 		
 		//2
 		String[] names = new String[] {"Sam", "Tommy", "Tim", "Sally", "Buck", "Bob"};
 		//2.a
-		System.out.println("2.a \n ---Rounded average character length of names: \n\t" + CalculateAverageNameLength(names));
+		System.out.println("2.a \n ---Rounded average character length of names:");
+		System.out.println("\tArray used: " + Arrays.toString(names));
+		System.out.println("\tResult: " + CalculateAverageNameLength(names));
+		System.out.print("   ----- Press Enter to continue-----");
 		if (pauses) {System.in.read();}
 		if (pauses) {System.in.read();}
 		
 		//2.b
-		System.out.println("2.b \n ---Concatenated names separated by spaces: \n\t" + ConcateNames(names));
+		System.out.println("2.b \n ---Concatenated names separated by spaces:");
+		System.out.println("\tArray used: " + Arrays.toString(names));
+		System.out.println("\tResult: " + ConcateNames(names));
+		System.out.print("   ----- Press Enter to continue-----");
 		if (pauses) {System.in.read();}
 		if (pauses) {System.in.read();}
 		
@@ -42,6 +57,7 @@ public class Week4Assignment {
 		System.out.println("\tYou must do the length property minus 1 however because the index positions are 0 based, but the length is not.");
 		System.out.println("\tArray used: " + Arrays.toString(names));
 		System.out.println("\t" + "EXAMPLE:\tnames[names.length - 1] \treturns: " + names[names.length - 1]);
+		System.out.print("   ----- Press Enter to continue-----");
 		if (pauses) {System.in.read();}
 		if (pauses) {System.in.read();}
 		
@@ -50,6 +66,7 @@ public class Week4Assignment {
 		System.out.println("\tArrays are 0 based and if they have been initialized, will always start with 0.");
 		System.out.println("\tArray used: " + Arrays.toString(names));
 		System.out.println("\t" + "EXAMPLE:\tnames[0] \treturns: " + names[0]);
+		System.out.print("   ----- Press Enter to continue-----");
 		if (pauses) {System.in.read();}
 		if (pauses) {System.in.read();}
 		
@@ -60,6 +77,7 @@ public class Week4Assignment {
 		System.out.println("\tFirst initialized: " + Arrays.toString(nameLengths));
 		BuildNameLengthsArray(nameLengths, names);
 		System.out.println("\tAfter setting values: " + Arrays.toString(nameLengths));
+		System.out.print("   ----- Press Enter to continue-----");
 		if (pauses) {System.in.read();}
 		if (pauses) {System.in.read();}
 		
@@ -67,6 +85,7 @@ public class Week4Assignment {
 		System.out.println("6 \n ---Total characters used in names array:");
 		System.out.println("\tUsing names array: " + Arrays.toString(names));
 		System.out.println("\t" + SumIntArray(nameLengths));
+		System.out.print("   ----- Press Enter to continue-----");
 		if (pauses) {System.in.read();}
 		if (pauses) {System.in.read();}
 		
@@ -76,6 +95,7 @@ public class Week4Assignment {
 		System.out.println("7 \n ---Method to concatenate a word a specified number of times: ");
 		System.out.println("\tEXAMPLE:\t word: " + myWord + " \t repeated: " + wordTimes + " times");
 		System.out.println("\t - " + ConcateWord(myWord, wordTimes));
+		System.out.print("   ----- Press Enter to continue-----");
 		if (pauses) {System.in.read();}
 		if (pauses) {System.in.read();}
 		
@@ -86,13 +106,15 @@ public class Week4Assignment {
 		System.out.println("\tFirst Name: " + firstName);
 		System.out.println("\tLast Name: " + lastName);
 		System.out.println("\t\tConcatenated: " + FullName(firstName, lastName));
+		System.out.print("   ----- Press Enter to continue-----");
 		if (pauses) {System.in.read();}
 		if (pauses) {System.in.read();}
 		
 		//9
 		System.out.println("9 \n ---Is the array greater than 100?" );
 		System.out.println("\tused the ages2 array for example: " + Arrays.toString(ages2) + "\tSum: " + SumIntArray(ages2));
-		System.out.println("\t" + IsArrayGreaterThan100(ages2));
+		System.out.println("\tResult: " + IsArrayGreaterThan100(ages2));
+		System.out.print("   ----- Press Enter to continue-----");
 		if (pauses) {System.in.read();}
 		if (pauses) {System.in.read();}
 		
@@ -100,7 +122,8 @@ public class Week4Assignment {
 		double[] myDoubles = {15.5,13.4,1.5,37.4,6.1,8.8,4.68,61.3};
 		System.out.println("10 \n ---Average of an array of doubles:");
 		System.out.println("\tArray used: " + Arrays.toString(myDoubles));
-		System.out.println("\t" + AverageOfDoubleArray(myDoubles));
+		System.out.println("\tResult: " + AverageOfDoubleArray(myDoubles));
+		System.out.print("   ----- Press Enter to continue-----");
 		if (pauses) {System.in.read();}
 		if (pauses) {System.in.read();}
 		
@@ -109,7 +132,8 @@ public class Week4Assignment {
 		System.out.println("11 \n ---Is the first array average greater than the second?");
 		System.out.println("\tArray 1 used: " + Arrays.toString(myDoubles) + "\t Average: " + AverageOfDoubleArray(myDoubles));
 		System.out.println("\tArray 2 used: " + Arrays.toString(myDoubles2) + "\t Average: " + AverageOfDoubleArray(myDoubles2));
-		System.out.println("\t\tGreater? " + Array1Geater(myDoubles, myDoubles2));
+		System.out.println("\tResult: " + Array1Geater(myDoubles, myDoubles2));
+		System.out.print("   ----- Press Enter to continue-----");
 		if (pauses) {System.in.read();}
 		if (pauses) {System.in.read();}
 		
@@ -119,7 +143,8 @@ public class Week4Assignment {
 		System.out.println("12 \n ---Will I buy a drink?");
 		System.out.println("\tFirst condition - is it hot outside: " + isHotOutside);
 		System.out.println("\tSecond condition - do I have enough money: " + moneyInPocket);
-		System.out.println("\t\tBuy Dink? \t" + WillBuyDrink(isHotOutside, moneyInPocket));
+		System.out.println("\tResult: - Buy Dink? \t" + WillBuyDrink(isHotOutside, moneyInPocket));
+		System.out.print("   ----- Press Enter to continue-----");
 		if (pauses) {System.in.read();}
 		if (pauses) {System.in.read();}
 		
@@ -128,7 +153,8 @@ public class Week4Assignment {
 		System.out.println("13 \n ---Create my own method");
 		System.out.println("\tThis method creates an array of random integers between 0 and 100. Pass the size of the array that you want.");
 		System.out.println("\tI created this method to create int arrays instead of initializing them myself");
-		System.out.println("\t\tCreated array: " + Arrays.toString(CreateIntArray(arrSize)));
+		System.out.println("\tResult:\tCreated array: " + Arrays.toString(CreateIntArray(arrSize)));
+		System.out.print("   ----- Press Enter to continue-----");
 		if (pauses) {System.in.read();}
 		if (pauses) {System.in.read();}
 	}
